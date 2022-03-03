fdd6de8a0e89eba6557ada4b1c965dcb2fad65f1
Alaister Young <a@alaisteryoung.com>
2022-03-03 17:24:38 +1100

    Adds realtime listener for sessions

--- example/pages/account/index.tsx
+++ example/pages/account/index.tsx
@@ -1,11 +1,54 @@
+import { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
 import AuthenticatedLayout from '../../components/AuthenticatedLayout'
 import Session from '../../components/Session'
 import Spinner from '../../components/Spinner'
 import { useSessionsQuery } from '../../lib/api/sessions'
+import { useIsLoggedIn } from '../../lib/auth'
+import supabase from '../../lib/supabase'
 import { NextPageWithLayout } from '../../lib/types'
 
 const AccountPage: NextPageWithLayout = () => {
+  const queryClient = useQueryClient()
+  const isLoggedIn = useIsLoggedIn()
+
   const { data, isLoading } = useSessionsQuery()
+
+  useEffect(() => {
+    console.log('hi')
+    const subscription = supabase
+      .from(`sessions`)
+      .on('*', (payload) => {
+        queryClient.setQueriesData(['sessions'], (oldData) => {
+          console.log('payload:', payload)
+          console.log('oldData:', oldData)
+          // const update = (entity: any) => {
+          //   if (payload.eventType === 'INSERT') {
+          //     return {
+          //       comments: [...entity.comments, payload.new],
+          //     }
+          //   }
+
+          //   if (payload.eventType === 'DELETE') {
+          //     return {
+          //       comments: entity.comments.filter(
+          //         (comment: any) => comment.id !== payload.old.id
+          //       ),
+          //     }
+          //   }
+          // }
+
+          // return Array.isArray(oldData) ? oldData.map(update) : update(oldData)
+          return oldData
+        })
+      })
+      .subscribe()
+
+    return () => {
+      supabase.removeSubscription(subscription)
+    }
+    // Re-run on login changes
+  }, [isLoggedIn])
 
   return (
     <div className="mb-20 space-y-6">
